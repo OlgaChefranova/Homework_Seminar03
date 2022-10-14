bd0881fc6a5126b93ac2de41ac6268a643f69324
OLGA <ochefranova@yandex.ru>
2022-10-14 21:54:26 +0500

    задача

---
+++ Exersice19/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число!");
+int num = int.Parse(Console.ReadLine()!);
+int result = num / 10000;
+Console.WriteLine($"{result}");
+
+
+
